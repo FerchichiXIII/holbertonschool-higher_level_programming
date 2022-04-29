219a7dfc1a20cbfcc0c655ba096a8c51b5442328
FerchichiXIII <anasferchichi22@gmail.com>
2022-04-29 20:02:49 +0100

    islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from pickle import TRUE
+from this import d
+
+
+def islower(c):
+    for i in c:
+        if c.islower():
+            return True
+        else:
+            return False
