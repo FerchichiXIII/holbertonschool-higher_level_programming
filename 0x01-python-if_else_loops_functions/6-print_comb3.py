7f7387ba46cf7be46b106b7409777c049bc6022d
FerchichiXIII <anasferchichi22@gmail.com>
2022-04-29 13:37:08 +0100

    comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in range(0, 9):
-    for b in range(a+1, 10):
-        if b < 9:
+    for b in range(a, 10):
+        if b > a and a < 8: 
             print("{}{}".format(a, b), end=', ')
 print("{}{}".format(a, b))
