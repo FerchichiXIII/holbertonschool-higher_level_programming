672a2e0101db96a795a3224a7972aacbf7de3897
FerchichiXIII <anasferchichi22@gmail.com>
2022-05-02 17:27:24 +0100

    print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-print_list_integer = __import__('0-print_list_integer').print_list_integer
-def print_list_integer(my_list=[]):
-    print(*my_list, sep="\n")
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        print(*my_list, sep="\n")
