bf99b61a24dc845f2bc88ac118091b93e3280c80
FerchichiXIII <anasferchichi22@gmail.com>
2022-05-05 19:20:56 +0100

    square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for i in matrix:
-        return [i ** 2 for i in matrix]
+    if matrix:
+        return [[i ** 2 for i in j]for j in matrix]
