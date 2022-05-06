777db47b8acd1ceab53e6308f1cdef84a3e24e93
FerchichiXIII <anasferchichi22@gmail.com>
2022-05-06 10:38:20 +0100

    uniq

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,7 @@
 #!/isr/bin/python3
-from re import A
-
-from zmq import NULL
-
-
 def uniq_add(my_list=[]):
     n = set(my_list)
-    a = NULL
+    a = 0
     for i in n:
         a = a+i
     return a
