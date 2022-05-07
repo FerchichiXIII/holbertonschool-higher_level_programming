6b4acbd331f8032870097d28cfa51ee6ca6bcb78
FerchichiXIII <anasferchichi22@gmail.com>
2022-05-07 17:25:22 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return None
-    mmax_key = max(a_dictionary, key=a_dictionary.get)
-    return mmax_key
+    else:
+        mmax_key = max(a_dictionary, key=a_dictionary.get)
+        return mmax_key
