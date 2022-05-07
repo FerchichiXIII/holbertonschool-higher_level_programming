00cf8df69e09b2de2b70ca2fffff986c0e5ddead
FerchichiXIII <anasferchichi22@gmail.com>
2022-05-07 16:22:30 +0100

    best key

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    else:
+        mmax_key = max(a_dictionary, key=a_dictionary.get)
+        return mmax_key
