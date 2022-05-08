430f479efc6ce621348347a926e92bc38605fc66
FerchichiXIII <anasferchichi22@gmail.com>
2022-05-08 19:25:06 +0100

    roman num

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,14 @@ def roman_to_int(roman_string):
     if roman_string == None:
         return 0
     num = 0
-    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_num = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in roman_string:
         num += roman_num.get(i, 0)
+    sub = {"CM": -200, "XC": -20, 'IX': -2}
+    char = ""
+    for i in range(0, len(roman_string)-1):
+        char = roman_string[i] + roman_string[i+1]
+        if char in sub:
+            num += sub[char]
     return num
