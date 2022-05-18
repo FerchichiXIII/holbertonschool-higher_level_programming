1b1efe3bf748b0995a39a3faab29ede2f48b07cb
FerchichiXIII <anasferchichi22@gmail.com>
2022-05-18 20:41:10 +0100

    task3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,6 +4,7 @@ def safe_print_division(a, b):
         res = a / b
     except ZeroDivisionError:
         res = None
+        return None
     finally:
         print("Inside result:{}".format(res))
     return res
