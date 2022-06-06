6434810dcdaf01285e4ebe4854fac6d924ee3a46
FerchichiXIII <anasferchichi22@gmail.com>
2022-06-06 15:43:31 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@ class Base:
 
     def __init__(self, id=None):
         """define the method parameter"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
