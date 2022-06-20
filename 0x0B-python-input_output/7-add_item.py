e5ba823a662cd296b3bf15afbdf7b2d916d35ba5
FerchichiXIII <anasferchichi22@gmail.com>
2022-06-20 13:58:46 +0100

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" in put output """
+""" input output """
 
 
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
 
 filename = "add_item.json"
 
