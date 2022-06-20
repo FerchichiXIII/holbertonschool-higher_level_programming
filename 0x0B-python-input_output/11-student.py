7bae1a0200f54811e34b4597f73e30ba1dfec42b
FerchichiXIII <anasferchichi22@gmail.com>
2022-06-20 22:02:32 +0100

    10-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+# usr/bin/python3
+"""input output"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """convert object to json"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
+
+    def reload_from_json(self, json):
+        """reload object from json"""
+        for key, value in json.items():
+            self.__dict__[key] = value
