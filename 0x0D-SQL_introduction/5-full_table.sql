a2cbecd2d66da67f1ce6beac8009ac334b299438
FerchichiXIII <anasferchichi22@gmail.com>
2022-07-05 20:06:14 +0100

    full table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- prints the full description of thetable first_tablefrom    the database hbtn_0c_0 in your MySQL server.
 
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+SHOW CREATE TABLE first_table;
