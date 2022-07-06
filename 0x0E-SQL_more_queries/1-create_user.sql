8bfab9a43a682a2ce478ddeacc62f69275a68019
FerchichiXIII <anasferchichi22@gmail.com>
2022-07-06 17:39:16 +0100

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Creates the MySQL server user user_0d_1.
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd ';
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
