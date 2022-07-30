4d810cf01ea51aa73b86cb2242d24238c41e1ed6
FerchichiXIII <anasferchichi22@gmail.com>
2022-07-30 14:57:39 +0100

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,8 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-  id INT UNIQUE NOT NULL AUTO_INCREMENT,
-  name VARCHAR(256) NOT NULL,
-  state_id INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
-);
+	PRIMARY KEY (id),
+	`id`	INT	NOT NULL AUTO_INCREMENT,
+	`state_id`	INT	NOT NULL,
+	`name`	VARCHAR(256)	NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
