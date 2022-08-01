50ef9f55ff500ecaf80bdd304fd1a189602835ba
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-02 00:03:00 +0100

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const txt = 'C id fun';
+const x = 'C id fun';
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log(txt);
+    console.log(x);
   }
 }
