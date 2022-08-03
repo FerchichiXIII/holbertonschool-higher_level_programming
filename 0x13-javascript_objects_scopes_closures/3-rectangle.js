4c0a435492f39ea1dd121ffccb2e6aa15c18b7c7
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-03 16:00:29 +0100

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
     for (let i = 0; i < this.height; i++) {
       let line = '';
       for (let j = 0; j < this.width; j++) {
-        line += '#';
+        line += 'X';
       }
       console.log(line);
     }
