2a780e45358a36bfdd48b1b71f2ef94007c43ad6
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-03 23:19:26 +0100

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else if (c === undefined) {
+      for (let j = 0; j < this.height; j++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
