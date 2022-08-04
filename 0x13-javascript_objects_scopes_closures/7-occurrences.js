562dccfe135b7691c450ba7c1cd9aa1f67652cfd
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-04 10:54:04 +0100

    Occurrences fix semistandard

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,7 +4,7 @@ exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; list.length > i; i++) {
     if (list[i] === searchElement) {
-      count ++;
+      count++;
     }
   }
   return count;
