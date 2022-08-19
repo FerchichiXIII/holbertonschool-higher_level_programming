ff4b2738ac91e054ea049ba04428a60f656453e0
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-19 18:15:44 +0100

    fix pycodestyle task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
- takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).
+ takes in a URL, sends a request to the URL and
+  displays the body of the response (decoded in utf-8).
 """
 
 from urllib import request, error
