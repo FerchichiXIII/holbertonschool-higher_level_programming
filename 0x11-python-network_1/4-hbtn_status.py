5a514682ec5818f18c42152a238391b25ecf7500
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-19 18:35:12 +0100

    fix task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,12 +3,10 @@
  fetches https://intranet.hbtn.io/status
 """
 
-from urllib import request
+import requests
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    response = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
