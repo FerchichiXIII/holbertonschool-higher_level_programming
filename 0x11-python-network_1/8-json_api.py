0f8f65fc61d249ac1c2e20e4da8384a1db70dfd3
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-19 18:53:18 +0100

    fix task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,12 +5,12 @@ takes in a letter and sends a POST request
 """
 
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if len(argv) > 1:
-        q = argv[1]
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
     else:
         q = ""
     data = {'q': q}
