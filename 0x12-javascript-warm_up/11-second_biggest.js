683889f92decb43de45a0f5dc666d23961d87e68
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-27 12:21:55 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+const { argv } = require('yargs');
+if (!argv[3]) {
   console.log(0);
 } else {
-  const arr = process.argv.sort();
-  console.log(arr.reverse()[1]);
+  argv.shift();
+  argv.shift();
+  const arr = argv.map(x => parseInt(x));
+  arr.sort((a, b) => b - a);
+  arr.pop();
+  console.log(arr[arr.length - 1]);
 }
