2d44068212e6bf23c420efdb2478467103557dbf
FerchichiXIII <anasferchichi22@gmail.com>
2022-08-27 22:52:16 +0100

    task6

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY '{}' \
-                ORDER BY id ASC".format(argv[4]))
+        "SELECT * FROM states WHERE name LIKE %s",
+        (argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,11 +3,11 @@
 contains the class definition
  of a State and an instance Base = declarative_base()
 """
-import sqllalchemy
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-base = declarative_base
+base = declarative_base()
 
 
 class State (base):
