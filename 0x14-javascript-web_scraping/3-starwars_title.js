3f3a778c97e98a102744b3c19a13fdca01d37b8f
root <root@DESKTOP-L7AG08S.localdomain>
2022-09-13 23:20:32 +0100

    Loripsum

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,10 +5,11 @@ const request = require('request');
 const argv = process.argv;
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
-request.get(url + argv[2], function (err, reponse, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+request.get (url + argv[2], function(err, reponse, body){
+    if (err){
+        console.log(err)
+    }
+    else {
+        console.log(JSON.parse(body).title);
+    }
 });
