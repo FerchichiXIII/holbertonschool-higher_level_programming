761801787d7106a42bf1d9122c43a21c639d682f
root <root@DESKTOP-L7AG08S.localdomain>
2022-09-13 11:43:47 +0100

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 // writes a string to a file.
-
-
+const fs = require('fs');
+fs.writeFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
